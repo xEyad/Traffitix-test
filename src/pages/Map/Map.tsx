b073261553d1331925c6c068b8a24e228b0b751f
Eyad <eyadhisham@yahoo.com>
2023-11-20 22:39:24 +0200

    working tab

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,12 +1,26 @@
 import { FunctionComponent, useState } from "react";
 import MapHeader from "./MapHeader/MapHeader";
+import MapTabs from "./MapTabs/MapTabs";
+import styles from "./Map.module.scss";
 interface MapProps {}
 
 const Map: FunctionComponent<MapProps> = () => {
   return (
-    <div>
+    <div className={styles.mapPage}>
       <MapHeader />
-      maaaaaaaaaaaap
+      <MapTabs />
+      <div style={{ width: "100%" }}>
+        <iframe
+          width="100%"
+          height="600"
+          scrolling="no"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        >
+          <a href="https://www.maps.ie/population/">
+            Calculate population in area
+          </a>
+        </iframe>
+      </div>
     </div>
   );
 };
---
+++ src/pages/Map/MapTabs/MapTabs.tsx
@@ -0,0 +1,58 @@
+import { FunctionComponent, useState } from "react";
+import styles from "./MapTabs.module.scss";
+import Switch from "react-switch";
+interface MapTabsProps {}
+
+const MapTabs: FunctionComponent<MapTabsProps> = () => {
+  const tabs = [{ label: "Tab1" }, { label: "Tab2" }, { label: "Tab3" }];
+  const [isHeatMapOn, setisHeatMapOn] = useState(false);
+  const [activeTab, setactiveTab] = useState(1);
+  const onFlowHeatMapClicked = () => {
+    setisHeatMapOn(!isHeatMapOn);
+  };
+  const onTabSelected = (idx: number) => {
+    setactiveTab(idx);
+  };
+  return (
+    <div className={styles.mapTabs}>
+      <div className={styles.tabs}>
+        {tabs.map((tab, idx) => (
+          <div
+            style={{
+              color: activeTab == idx ? "#F2AB19" : "",
+              textUnderlineOffset: "8px",
+              textDecoration: activeTab == idx ? "underline" : "",
+            }}
+            className={styles.tab}
+            onClick={() => onTabSelected(idx)}
+          >
+            {tab.label}
+          </div>
+        ))}
+      </div>
+      <div className={styles.heatBtn}>
+        <span>Flow heat maps</span>
+        <div style={{ paddingTop: "11px" }}>
+          <Switch
+            width={34}
+            height={18}
+            onColor="#fdf7e9"
+            checkedIcon={false}
+            uncheckedIcon={false}
+            onHandleColor="#F2AB19"
+            className="react-switch"
+            onChange={onFlowHeatMapClicked}
+            checked={isHeatMapOn}
+            aria-labelledby="neat-label"
+          />
+        </div>
+      </div>
+      <div className={styles.incidentBtn}>
+        <div></div>
+        <span>Incident data</span>
+      </div>
+    </div>
+  );
+};
+
+export default MapTabs;
